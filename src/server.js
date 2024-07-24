ff8c2cce2e2577ab9c8d951f11117ba452084c36
Walyson Felix <walysonfelix1991@gmail.com>
2024-07-24 17:14:04 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log('Boas vindas a API aLLbOOKS')
+  console.log("API disponível atravez de http://localhost:8000")
 })
